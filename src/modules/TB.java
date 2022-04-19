20816271c0369f683402b46ae76375cd9ba94a69
goehringng <goehringng@vcu.edu>
2022-04-19 13:12:40 -0400

    added new files

---
+++ src/modules/TB.java
@@ -0,0 +1,103 @@
+/****************************************************************************************
+ TextBroker Class
+ ***************************************************************************************
+ Function:
+ Retrieves information from text files for taxes, errors, or translations.
+ *--------------------------------------------------------------------------------------
+ @author Nathan Goehring
+ @version 04/19/2022   CMSC 355
+  * ***************************************************************************************/
+
+import java.io.*;
+
+public class TB {
+    public static void main(String[] args) throws Exception {
+        String[] b = args[0].split(",");
+        String line;
+        File file = new File(
+                "src/textfiles/" + b[0]);
+        BufferedReader a = null;
+        if (b.length > 2) {
+            try {
+                a = new BufferedReader(new FileReader(file));
+            } catch (FileNotFoundException e) {
+                Process pb = new ProcessBuilder("java", "src/modules/Error.java", "903").start();
+                try (var reader = new BufferedReader(new InputStreamReader(pb.getInputStream()))) {
+                    String l;
+                    while ((l = reader.readLine()) != null) {
+                        System.out.println(l);
+                    }
+                }
+                System.exit(0);
+            }
+            if (b[2].equals("<=")) {
+                int userGross = 0;
+                try {
+                    userGross = Integer.parseInt(b[1]);
+                } catch (NumberFormatException e) {
+                    Process pb = new ProcessBuilder("java", "src/modules/Error.java", "404").start();
+                    try (var reader = new BufferedReader(new InputStreamReader(pb.getInputStream()))) {
+                        String l;
+                        while ((l = reader.readLine()) != null) {
+                            System.out.println(l);
+                        }
+                    }
+                    System.exit(0);
+                }
+                if (userGross <= 0) {
+                    Process pb = new ProcessBuilder("java", "src/modules/Error.java", "404").start();
+                    try (var reader = new BufferedReader(new InputStreamReader(pb.getInputStream()))) {
+                        String l;
+                        while ((l = reader.readLine()) != null) {
+                            System.out.println(l);
+                        }
+                    }
+                    System.exit(0);
+                }
+                while ((line = a.readLine()) != null) {
+                    String[] words = line.split(",");
+                    int incomeAmount = 0;
+                    try {
+                        incomeAmount = Integer.parseInt(words[0]);
+                    } catch (NumberFormatException e) {
+                        System.out.println(words[1]);
+                        System.exit(0);
+                    }
+                    if (userGross <= incomeAmount) {
+                        System.out.println(words[1]);
+                        System.exit(0);
+                    }
+                }
+            }
+        }
+        try {
+            a = new BufferedReader(new FileReader(file));
+        } catch (FileNotFoundException e) {
+            Process pb = new ProcessBuilder("java", "src/modules/Error.java", "805").start();
+            try (var reader = new BufferedReader(new InputStreamReader(pb.getInputStream()))) {
+                String l;
+                while ((l = reader.readLine()) != null) {
+                    System.out.println(l);
+                }
+            }
+            System.exit(0);
+        }
+        while ((line = a.readLine()) != null) {
+            String[] words = line.split(",");
+            if (words[0].equals(b[1])) {
+                System.out.println(words[1]);
+                System.exit(0);
+            }
+        }
+            Process pb = new ProcessBuilder("java", "src/modules/Error.java", "813").start();
+            try (var reader = new BufferedReader(new InputStreamReader(pb.getInputStream()))) {
+                String l;
+                while ((l = reader.readLine()) != null) {
+                    System.out.println(l);
+                }
+            }
+            System.exit(0);
+        }
+    }
+
+
---
+++ src/modules/Tax.java
@@ -0,0 +1,47 @@
+/****************************************************************************************
+ Tax Class
+ ***************************************************************************************
+ Function:
+
+ *--------------------------------------------------------------------------------------
+ @author Anisha Beladia & Nathan Goehring
+ @version 04/19/2022   CMSC 355
+  * ***************************************************************************************/
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Tax {
+    public static void main(String[] args) throws Exception {
+        String[] tax = args[0].split(",");
+        int year = Integer.parseInt(tax[0]);
+        String type = tax[1];
+        int userGross = Integer.parseInt(tax[2]);
+        switch (type) {
+            case "Single" -> type = "S";
+            case "Joint" -> type = "J";
+            case "Head" -> type = "H";
+            default -> {
+                Process pb = new ProcessBuilder("java", "src/modules/Error.java", "876").start();
+                try (var reader = new BufferedReader(new InputStreamReader(pb.getInputStream()))) {
+                    String l;
+                    while ((l = reader.readLine()) != null) {
+                        System.out.println(l);
+                    }
+
+                }
+                System.exit(0);
+            }
+        }
+        String filer = year + type + ".txt";
+        String set = "TB" + "," + filer + "," + userGross + "," + "<=";
+        Process pb = new ProcessBuilder("java", "src/modules/SB.java", set).start();
+        try (var reader = new BufferedReader(new InputStreamReader(pb.getInputStream()))) {
+            String l;
+            while ((l = reader.readLine()) != null) {
+                double percentage = Double.parseDouble(l);
+                double taxes = userGross * percentage;
+                System.out.print(taxes);
+            }
+        }
+    }
+}
