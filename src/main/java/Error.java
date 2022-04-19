00d40c25bb07fe9018218a0508f15750830dd4fb
goehringng <goehringng@vcu.edu>
2022-04-19 13:25:23 -0400

    added new functionality

--- src/main/java/Error.java
+++ src/main/java/Error.java
@@ -21,15 +21,32 @@ public class Error {
         //args will be Language,message-code
         //need to split args to get Language and then message code
         String[] argsArray = args[0].split(",");
-        //int i = 0;
-        Process pb = new ProcessBuilder("java", "src/main/java/SB.java", "TB,msgEng.txt," + argsArray[1]).start();
+        String language;
+        int code = 404;
+        String filer = "msgEng.txt";
+        String set;
+        String line;
+        // default language file to English if one is not provided
+        if (argsArray.length == 1) {
+            code = Integer.parseInt(argsArray[0]);
+        }
+        else if (argsArray.length == 2){
+            language = argsArray[0].substring(0, 3);
+            code = Integer.parseInt(argsArray[1]);
+            // format language.txt file
+            filer = "msg" + language + ".txt";
+        }
+        // format arguments for calling SB
+        set = "TB" + "," + filer + "," +  code;
+        // call SB using service-oriented call
+        Process pb = new ProcessBuilder("java", "src/modules/SB.java", set).start();
+        // read input from pb
         try (var reader = new BufferedReader(new InputStreamReader(pb.getInputStream()))) {
-            String l;
-            while ((l = reader.readLine()) != null) {
-                System.out.println(l);
+            while ((line = reader.readLine()) != null) {
+                // print error message to user
+                System.out.println(line);
             }
         }
-        System.exit(0);
     }
 }
 
