6c159d7af1a8f7e5f29fea24adb48a208c271b16
Parkmj5 <69889003+Parkmj5@users.noreply.github.com>
2022-04-21 10:23:26 -0400

    Update Error.java
    added comment blocks

--- src/modules/Error.java
+++ src/modules/Error.java
@@ -19,8 +19,8 @@ import java.util.*;
 public class Error {
     public static void main(String[] args) throws Exception {
         /*-------------------------------------------------------------
-        //args will be Language,message-code
-        //need to split args to get Language and then message code
+        // args will be Language,message-code
+        // need to split args to get Language and then message code
         *-------------------------------------------------------------*/
         String[] argsArray = args[0].split(",");
         String language;
@@ -37,17 +37,27 @@ public class Error {
         else if (argsArray.length == 2){
             language = argsArray[0].substring(0, 3);
             code = Integer.parseInt(argsArray[1]);
+            /*-------------------------------------------------------------
             // format language.txt file
+            *-------------------------------------------------------------*/
             filer = "msg" + language + ".txt";
         }
+        /*-------------------------------------------------------------
         // format arguments for calling SB
+        *-------------------------------------------------------------*/
         set = "TB" + "," + filer + "," +  code;
+        /*-------------------------------------------------------------
         // call SB using service-oriented call
+        *-------------------------------------------------------------*/
         Process pb = new ProcessBuilder("java", "src/modules/SB.java", set).start();
+        /*-------------------------------------------------------------
         // read input from pb
+        *-------------------------------------------------------------*/
         try (var reader = new BufferedReader(new InputStreamReader(pb.getInputStream()))) {
             while ((line = reader.readLine()) != null) {
+                /*-------------------------------------------------------------
                 // print error message to user
+                *-------------------------------------------------------------*/
                 System.out.println(line);
             }
         }
